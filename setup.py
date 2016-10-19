041fa960732137ee091a36194197e6c917317b43
Jon Robison <narfman0@gmail.com>
2016-10-18 22:19:42 -0400

    -Remvoe parse again of course :)

--- setup.py
+++ setup.py
@@ -1,4 +1,3 @@
-from parse_requirements_not_suckily import parse_requirements
 from setuptools import setup, find_packages
 from helga_craigslist_meta import __version__ as version
 
@@ -23,7 +22,9 @@ setup(
     include_package_data=True,
     py_modules=['helga_craigslist_meta.plugin'],
     zip_safe=True,
-    install_requires=parse_requirements(),
+    install_requires=[
+        'craiglist_scraper',
+    ],
     test_suite='',
     entry_points=dict(
         helga_plugins=[
