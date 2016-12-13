69b25f5bf2fcea6cb7bc254817cadb3c9795b311
Jon Robison <narfman0@gmail.com>
2016-12-13 10:05:51 -0500

    -Minor req update

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     py_modules=['helga_craigslist_meta.plugin'],
     zip_safe=True,
     install_requires=[
-        'craiglist_scraper',
+        'craiglist-scraper',
     ],
     test_suite='',
     entry_points=dict(
